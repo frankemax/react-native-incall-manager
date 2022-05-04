53e4fb0133c739cadbf3e3bd7e18ecc585e2d6a3
Tomasz Baranowicz <tbaranowicz@gmail.com>
2022-05-04 15:09:23 +0200

    Do not reset Speaker selection on stop/start

--- ios/RNInCallManager/RNInCallManager.m
+++ ios/RNInCallManager/RNInCallManager.m
@@ -140,7 +140,6 @@ RCT_EXPORT_METHOD(start:(NSString *)mediaType
     }
     NSLog(@"RNInCallManager.start() start InCallManager. media=%@, type=%@, mode=%@", _media, _media, _incallAudioMode);
     [self storeOriginalAudioSetup];
-    _forceSpeakerOn = 0;
     [self startAudioSessionNotification];
     [self audioSessionSetCategory:_incallAudioCategory
                           options:0
@@ -187,7 +186,6 @@ RCT_EXPORT_METHOD(stop:(NSString *)busytoneUriType)
         [self setKeepScreenOn:NO];
         [self stopAudioSessionNotification];
         [[NSNotificationCenter defaultCenter] removeObserver:self];
-        _forceSpeakerOn = 0;
         _audioSessionInitialized = NO;
     }
 }
